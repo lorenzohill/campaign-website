138e51d8afb7ebe691243fc6c06da310f6637b1d
Lorenzo Hill <lorenzokhill985@gmail.com>
2023-04-04 15:50:18 -0500

    Added menu for mobile

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMenu(){
+  const menuBtn = document.getElementById("menu")
+  const navMenu = document.querySelector("nav")
+
+  menuBtn.addEventListener("click", function(){
+      navMenu.classList.toggle("show")
+
+  }
+  )
+}
+
+
+showMenu()
